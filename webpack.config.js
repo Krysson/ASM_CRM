398fb29211d97e5fca0b94024884221a232ccffc
mgulla <mgulla@krysson.com>
2022-06-23 00:19:26 -0400

    Fixed Webpack HTML loader issue

--- webpack.config.js
+++ webpack.config.js
@@ -22,14 +22,8 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-react", ["@babel/preset-env", { targets: { node: "12" } }]]
-          }
-        }
+        test: /\.html$/i,
+        loader: "html-loader"
       }
     ]
   }
